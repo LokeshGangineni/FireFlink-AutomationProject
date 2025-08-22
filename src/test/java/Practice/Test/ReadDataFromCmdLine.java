fd7c60ad6eabc99ad7c8bce4d04252444905fc9c
Gangineni Lokesh <Gangineni Lokesh@FFE-Gangineni-Lokesh>
2025-08-22 15:08:43 +0530

    added url in ReadDataFromExcel

--- src/test/java/Practice/Test/ReadDataFromCmdLine.java
+++ src/test/java/Practice/Test/ReadDataFromCmdLine.java
@@ -2,17 +2,19 @@ package Practice.Test;
 
 import org.testng.annotations.Test;
 
-public class ReadDataFromCmdLine {
+public class  ReadDataFromCmdLine {
 		
 	@Test	
 	public void checking()
 	{
-		String BROWSER= System.getProperty("browser");
+		String BROWSER = System.getProperty("browser");
 		System.out.println(BROWSER);
 		String USERNAME = System.getProperty("username");
 		System.out.println(USERNAME);
 		String PWD= System.getProperty("password");
 		System.out.println(PWD);
+		String URL= System.getProperty("url");
+		System.out.println(URL);
 	}
 
 }
